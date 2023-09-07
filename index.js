5c4a33776f20a71cf774bdace2d8bbd813af7bdb
Antonio <antoniojm.2133@gmail.com>
2023-09-07 08:04:05 -0700

    Everything

--- index.js
+++ index.js
@@ -4,6 +4,14 @@
 // init project
 var express = require('express');
 var app = express();
+var mongoose = require('mongoose');
+require('dotenv').config();
+var port = process.env.PORT || 3000;
+
+mongoose.connect(process.env.DB_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
 
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
 // so that your API is remotely testable by FCC 
@@ -20,13 +28,56 @@ app.get("/", function (req, res) {
 
 
 // your first API endpoint... 
-app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
+// your first API endpoint... 
+app.get("/api/timestamp", (req, res)=>{
+  let date = new Date();
+  
+  return res.json({
+    'unix': date.getTime(), 
+    'utc': date.toUTCString()
+  });  
 });
 
+const isInvalidDate = (date) => date.toUTCString() === "Invalid Date";
 
+app.get("/api/:date", (req, res)=>{
+  let date = new Date(req.params.date);
+  
+  if(isInvalidDate(date)){
+    date = new Date(+req.params.date)
+  }
+
+  if(isInvalidDate(date)){
+    res.json({error: "Invalid Date"})
+    return;
+  }
+
+  res.json({
+    unix: date.getTime(),
+    utc: date.toUTCString()
+  });
+});
+
+app.get("/api", (req, res) => {
+  res.json({
+    unix: new Date().getTime(),
+    utc: new Date().toUTCString()
+  })
+})
+
+app.get("/api/timestamp/:date_str?", function(req, res){
+  const { date_str } = req.params;
+  console.log(date_str)
+  let date;
+
+    return res.json({
+      unix: date.getTime(),
+      utc: date.toUTCString()
+    })
+
+});
 
 // listen for requests :)
-var listener = app.listen(process.env.PORT, function () {
+var listener = app.listen(port, function () {
   console.log('Your app is listening on port ' + listener.address().port);
 });
